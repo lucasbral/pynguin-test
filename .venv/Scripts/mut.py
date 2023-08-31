5f39f7d131d6e58b6a13598b885d32ca24f7be3b
Lucas Sobral <lucasdecarvalhosobral@gmail.com>
2023-08-31 13:46:34 -0300

    v1

---
+++ .venv/Scripts/mut.py
@@ -0,0 +1,7 @@
+#!C:\Users\lucas\OneDrive\Área de Trabalho\Projeto Pynguin\.venv\Scripts\python.exe
+
+import sys
+from mutpy import commandline
+
+if __name__ == '__main__':
+    commandline.main(sys.argv)
